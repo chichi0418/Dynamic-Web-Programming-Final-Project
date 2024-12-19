4495292cd1124c333155222b7a017d698d330f7a
yyaannn <g853520ck@gmail.com>
2024-12-19 20:48:38 +0800

    fix: update database connection settings

--- backend/connection.php
+++ backend/connection.php
@@ -4,12 +4,14 @@
     header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type');
 
-    $host = '127.0.0.1';    # modifed
+    $host = '127.0.0.1';
     $data = 'accounting';
     $user = 'root';
-    $pass = 'root';     # modifed
+    $pass = '';
+    // $pass = 'root';
     $chrs = 'utf8mb4';
-    $attr = "mysql:host=$host;port=8890;dbname=$data;charset=$chrs";    # added port=8890
+    $attr = "mysql:host=$host;dbname=$data;charset=$chrs";
+    // $attr = "mysql:host=$host;port=8890;dbname=$data;charset=$chrs";
     $opts = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -22,7 +24,7 @@
         http_response_code(500);
         echo json_encode([
             "status" => "error",
-            "message" => $e->getMessage(),
+            "message" => "資料庫連線失敗！",
         ]);
         exit;
     }
