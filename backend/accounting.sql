e55de2ef8afcf1d951097a9adf5011bec030e487
yyaannn <g853520ck@gmail.com>
2024-12-22 22:44:24 +0800

    Modify database schema

--- backend/accounting.sql
+++ backend/accounting.sql
@@ -32,7 +32,8 @@ USE `accounting`;
 CREATE TABLE `accounts` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `balance` int(11) NOT NULL DEFAULT 0
+  `balance` int(11) NOT NULL DEFAULT 0,
+  `budget` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
